30650f2bf36c5c06e6c61f2daacad6e0274be0d8
hgaziuddin <hgaziuddin@students.mchenry.edu>
2016-09-08 21:57:12 -0500

    Add files via upload

---
+++ callFunction.py
@@ -0,0 +1,8 @@
+def sayRhyme():
+    print("Fuzzy Wuzzy was a bear")
+    print("Fuzzy Wuzzy had no hair")
+    print("Fuzzy Wuzzy wasn't really fuzzy, was he?\n")
+
+sayRhyme()
+sayRhyme()
+sayRhyme()
